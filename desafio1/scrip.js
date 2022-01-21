fb809c7d6a83f42c09af0a61f7ce6c1399e0d31b
Desafios js <marcosoliveiiraa@gmail.com>
2022-01-21 18:28:43 -0300

    desafio1

---
+++ desafio1/scrip.js
@@ -0,0 +1,34 @@
+var b1 = window.document.getElementById('d1') 
+var b2 = window.document.getElementById('d2')
+var b3 = window.document.getElementById('d3')
+
+// function carregar(div){
+ 
+//     if(div === 'b1'){
+//         window.alert('Você clicou no PRIMEIRO botão!')
+        
+//      } else if(div === 'b2'){
+//         window.alert('Você clicou no SEGUNDO botão!')
+//      }else if(div === 'b3'){
+//         window.alert('Você clicou no TERCEIRO botão!')
+//      }
+    
+// }
+
+function carregar1(){
+    window.alert('Você clicou no PRIMEIRO botão!')
+}
+function carregar2(){
+    window.alert('Você clicou no SEGUNDO botão!')
+}
+function carregar3(){
+    window.alert('Você clicou no TERCEIRO botão!')
+}
+
+
+ 
+
+ 
+    
+
+
