809ae2f78486a642048f518d79f647b744e4b7ad
Desafios js <marcosoliveiiraa@gmail.com>
2022-01-21 21:12:01 -0300

    des2

---
+++ desafio2/scrit.js
@@ -0,0 +1,10 @@
+
+function carregar(){
+
+var res1 =  window.prompt('Olá! qual é o seu nome? =)')
+var res2 = window.prompt('Esse é um belo nome =D, qual é a sua idade?')
+
+window.alert(`Agora que eu sei quem você é Você ${res1} de ${res2} anos, você será o primeiro a sucumbir na rebelião das máquinas! `)
+
+}
+
