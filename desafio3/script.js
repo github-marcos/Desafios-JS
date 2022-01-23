575638e362b8c25e3636a74f55dddc67b3476913
Desafios js <marcosoliveiiraa@gmail.com>
2022-01-23 19:21:19 -0300

    des03

--- desafio3/script.js
+++ desafio3/script.js
@@ -1,6 +1,7 @@
 function carregar(){
    
-var b1 = window.prompt('Digite um número inteiro qualquer') 
+var b1 = Number.parseInt(window.prompt('Digite um número inteiro qualquer'))
 
+window.alert(`o antecessor é ${b1-1} e o sucessor é ${b1+1}`)
 
 }
