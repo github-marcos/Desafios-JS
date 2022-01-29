efb047c9e4d95552843e0d0dcc208dc03dbe387f
Desafios js <marcosoliveiiraa@gmail.com>
2022-01-28 21:37:09 -0300

    des05

--- desafio5/script.js
+++ desafio5/script.js
@@ -1,8 +1,27 @@
 function carregar(){
   var medida1 =  Number.parseFloat(window.prompt('Digite uma medida em metros (m)'))
 
-  var medida2 = Number.parseFloat(window.document.getElementsByName( ` ${medida1 / 1000}`) + medida1.innerHTML)
+  var medida2 = window.document.getElementById('h2')
 
-  //Number.parseFloat(window.alert(`${medida2} km`))
+  medida2.innerHTML =  ` A distância de  ${medida1} metros, corresponde a: ${medida1 / 1000}KM`
 
+  var medida3 = window.document.getElementById('h3')
+  medida3.innerHTML = ` Hectômetros (Hm): ${medida1 / 100 } <br>
+  Decâmetros(Dam): ${medida1 / 10} <br>
+  Decímetros(dm): ${medida1 * 10}<br>
+  Centímetros(cm): ${medida1 * 100}<br>
+  Milímetros(mm): ${medida1 * 1000} `
+
+
+  /*var medida4 = window.document.getElementById('p')
+  medida3.innerHTML = ` Decâmetros(Dam): ${medida1 * 10}`
+
+  var medida5 = window.document.getElementById('p')
+  medida3.innerHTML = ` Decímetros(dm): ${medida1 / 10}`
+
+  var medida6 = window.document.getElementById('p')
+  medida3.innerHTML = ` Centímetros(Dam): ${medida1 / 100}`
+
+  var medida7 = window.document.getElementById('p')
+  medida3.innerHTML = ` Milímetros(mm): ${medida1 / 1000}`*/
 }
