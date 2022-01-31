d61b3360784e036486a2d86c4762e3054b552c4e
Desafios js <marcosoliveiiraa@gmail.com>
2022-01-31 15:48:45 -0300

    des6

--- desafio6/script.js
+++ desafio6/script.js
@@ -1,5 +1,15 @@
 function carregar(){
-window.prompt('Digite uma temperatura em °C (Celsios)')
 
+var res =  Number.parseFloat(window.prompt('Digite uma temperatura em °C (Celsios)'))
+
+ res2 = window.document.getElementById('h2') 
+
+res2.innerHTML = `A temperatura de ${res}°C, corresponde a...`
+
+var res3 = window.document.getElementById('h3')
+res3.innerHTML = (` ${res + 273,15}°K (kelvin)`)
+
+var res4 = window.document.getElementById('h4')
+res4.innerHTML = `${res * 9/5 + 32}°F (Fahrenheit)`
 
 }
