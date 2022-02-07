4e2883d38003486febbebfb1774b94829cee378d
Desafios js <marcosoliveiiraa@gmail.com>
2022-02-07 14:58:23 -0300

    des10

--- desafio10/script.js
+++ desafio10/script.js
@@ -7,9 +7,10 @@ var res3 = window.prompt('Qual o valor de c?')
   
 window.document.getElementById('h2').innerHTML = 'Resolvendo Bhaskara'
 
-window.document.getElementById('h3').innerHTML = `A equação atual é `
+window.document.getElementById('h3').innerHTML = `A equação atual é &#120607; = ${res}x<sup>2</sup> + ${res2}x + ${res3} = 0 `
 
-window.document.getElementById('h4').innerHTML = `O calculo realizado será `
+window.document.getElementById('h4').innerHTML = `O calculo realizado será &#120607; = ${res2}<sup>2</sup> - 4 . ${res} . ${res3}`   
 
-window.document.getElementById('h5').innerHTML = `O valor calculado foi `
+
+window.document.getElementById('h5').innerHTML = `O valor calculado foi &#120607; = <mark>${(res2**2) -4 *( res * res3) } </mark>`
 }
